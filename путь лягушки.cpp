a88555ea522301af6d595f3a86d4518bef6e1f96
Karpovich Yulia <375339008080.yuluya@gmail.com>
2025-10-01 23:36:46 +0300

    With way

--- путь лягушки.cpp
+++ путь лягушки.cpp
@@ -9,18 +9,16 @@ int main()
     cin >> n;
     vector<int> V(n+1);
     vector<int> work(n+1);
+    
+    for (int i = 1; i < n+1; i++) {
+        cin >> V[i];
+    }
     if (n == 2) {
         cout << -1;
         return 0;
     }
-    for (int i = 1; i < n+1; i++) {
-        cin >> V[i];
-    }
+   
     
-    work[1] = V[1];
-    work[2] = -100000;
-    work[3] = V[3] + V[1];
-    work[4] = V[4] + V[1];
     //work[5] = V[1] + V[3] + V[5];
     if (n == 1) {
         cout << V[1] << endl << 1;
@@ -30,13 +28,46 @@ int main()
         cout << V[1] + V[3] << endl << "1 3";
         return 0;
     }
+    work[1] = V[1];
+    work[2] = -100000;
+    work[3] = V[3] + V[1];
+    work[4] = V[4] + V[1];
 
-
+    vector<int> way(n + 1, -1);
     for (int i = 5; i <= n; i++) {
         
-        work[i] = max(work[i - 2], work[i - 3]) + V[i];
+        if (work[i - 2] > work[i - 3]) {
+            work[i] = work[i - 2] + V[i];
+            way[i] = i - 2;
+        }
+        else {
+            work[i] = work[i - 3] + V[i];
+            way[i] = i - 3;
+        }
+        //work[i] = max(work[i - 2], work[i - 3]) + V[i];
     }
-    cout << work[n];
+    cout << work[n] << endl;
+    /*for (int i = 1; i < n + 1; i++) {
+        cout << way[i] << ' ';
+    }
+    cout << endl;*/
+    vector <int> newWay;
+    int induk = n;
+    way[1] = 0;  
+    way[3] = 1; 
+    way[4] = 1;  
+
+    while (induk > 0) {
+        newWay.push_back(induk);
+        induk = way[induk];
+    }
+    reverse(newWay.begin(), newWay.end());
+
+    // Вывод результата
+    //cout << work[n] << endl;
+    for (int i = 0; i < newWay.size(); i++) {
+        cout << newWay[i] << ' ';
+    }
     
 
 
